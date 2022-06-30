e2c98a528613abca21704a5d0d0b9cb59b53e490
Zakir Hussain <zack.mesacc@gmail.com>
2022-06-30 04:42:50 -0500

    Fix more errorsss

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   end
 
   def create 
+    @user = User.new(user_params)
     if @user.save 
       session[:user_id] = @user.id
       redirect_to @user, notice: 'Thanks for signing up!'
