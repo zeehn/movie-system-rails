100a0aec6d8fae8187c059703387d5178482fdc3
Zakir Hussain <zack.mesacc@gmail.com>
2022-07-01 11:48:37 -0500

    Fix bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def main_image(movie)
     if movie.main_image.attached? 
-      image_tag movie.main_image.variant(resize_to_limit: [150, nil])
+      image_tag movie.main_image
     else
       image_tag("placeholder.png")
     end
